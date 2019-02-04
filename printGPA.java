5b3c4a3eafb893066070cd69afd1fefa80143686
smbarajas <32750643+smbarajas@users.noreply.github.com>
2019-02-04 10:46:28 -0800

    Update printGPA.java

--- printGPA.java
+++ printGPA.java
@@ -4,18 +4,21 @@ After typing a line of input which contains the student's name, user will then i
 of scores, followed by that number of integer scores.
 */
 
-public static void printGPA() {
+import java.util.Scanner;
+public class printGPA {
+  public static void printGPA(String[] args) {
     Scanner console = new Scanner(System.in);
     System.out.print("Please enter a student record: ");
     String name = console.next();
-    int numScore = console.next.Int();
+    double numScore = console.nextDouble();
     double sum = 0.0;
 
-    for (int i = 0; i < numScore; i++) {
-        int score = console.nextInt();
+    for (double i = 0.0; i < numScore; i++) {
+        double score = console.nextDouble();
         sum += score;
 
     }
     double average = sum / numScore;
     System.out.println(name + "'s grade point average is " + average);
+  }
 }
