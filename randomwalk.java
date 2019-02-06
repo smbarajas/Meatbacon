248d2319de52aa5655d4220029673d1c7c201aeb
smbarajas <32750643+smbarajas@users.noreply.github.com>
2019-02-06 14:45:35 -0800

    Update randomwalk.java
    Added comments to the code.

--- randomwalk.java
+++ randomwalk.java
@@ -1,3 +1,9 @@
+/* Write a randomWalk method that preforms steps of a random one-dimensional walk.
+The walk begins at 0 and will either increase or decrease the postion by one.
+There will be an equal probability for it to decrease and increase.
+It will continue making steps until a position of 3 or -3 is reached.
+It will then report the max position. */
+
 import java.util.Random;
 class Main {
 	public static void main(String[] args) {
