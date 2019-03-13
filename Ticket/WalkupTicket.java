ff7ea778a32952afac325defb3c2628a8a4655b0
smbarajas <32750643+smbarajas@users.noreply.github.com>
2019-03-13 12:22:45 -0700

    Create WalkupTicket.java

---
+++ Ticket/WalkupTicket.java
@@ -0,0 +1,13 @@
+/*
+Will hold the price of the walkup ticket (50 Dollars)
+*/
+
+package tester;
+
+public class WalkupTicket extends Ticket{
+
+public WalkupTicket(){
+    this.price=50;
+    ticket++;
+  }
+}
